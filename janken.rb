f86ebfa54a1e8748e88d7feb3bcb01096f21a266
EC2 Default User <ec2-user@ip-172-31-27-62.ap-northeast-1.compute.internal>
2022-01-29 15:12:40 +0000

    Rubyジャンケン提出

--- janken.rb
+++ janken.rb
@@ -4,51 +4,57 @@ def janken
   puts "じゃんけん..."
   puts "1(グー)2(チョキ)3(パー)4(戦わない)"
 
-  player_hand = gets.chomp.to_i
+  $player_hand = gets.chomp.to_i
   
-  if player_hand == 4
+  if $player_hand == 4
     puts "ゲームを終了します"
     return true
   end
-  if player_hand > 4 || player_hand < 1
+  if $player_hand > 4 || $player_hand < 1
     puts "エラー：正しい数値を入力してください"
     return true
   end
-  player_hand -= 1 
+  $player_hand -= 1 
   program_hand = rand(0..2)
-  if player_hand == 0 || player_hand == 1 || player_hand == 2 
+  if $player_hand == 0 || $player_hand == 1 || $player_hand == 2 
     puts "ポイ！"
-    puts "あなた：" + jankens[player_hand] + "を出しました"
+    puts "あなた：" + jankens[$player_hand] + "を出しました"
     puts "相手：" + jankens[program_hand] + "を出しました"
     
     puts "--------------"
-    while player_hand == program_hand
+    while $player_hand == program_hand
       puts "あいこで..."
       puts "1(グー)2(チョキ)3(パー)4(戦わない)"
-      player_hand += 1
-      player_hand = gets.chomp.to_i
-      if player_hand == 4
+      $player_hand += 1
+      $player_hand = gets.chomp.to_i
+      if $player_hand == 4
         puts "ゲームを終了します"
         return true
-      elsif player_hand > 3 || player_hand < 1
+      elsif $player_hand > 4 || $player_hand < 1
         puts "エラー：正しい数値を入力してください"
         return true
       else
-        player_hand -= 1
+        $player_hand -= 1
         program_hand = rand(0..2)
         puts "しょっ！"
-        puts "あなた：" + jankens[player_hand] + "を出しました"
+        puts "あなた：" + jankens[$player_hand] + "を出しました"
         puts "相手：" + jankens[program_hand] + "を出しました"
       
         puts "--------------"
       end
     end
-    if (player_hand == 0 && program_hand == 1) || (player_hand == 1 && program_hand == 2) || (player_hand == 2 && program_hand == 0)
+    if ($player_hand == 0 && program_hand == 1) || ($player_hand == 1 && program_hand == 2) || ($player_hand == 2 && program_hand == 0)
+      puts "勝ち"
+      puts "指の方向を選んでください"
+      puts "--------------"
       $janken_result = "win"
-      return false
-    elsif (player_hand == 0 && program_hand == 2) || (player_hand == 1 && program_hand == 0) || (player_hand == 2 && program_hand == 1)
+      $player_hand += 1
+    elsif ($player_hand == 0 && program_hand == 2) || ($player_hand == 1 && program_hand == 0) || ($player_hand == 2 && program_hand == 1)
+      puts "負け"
+      puts "顔の方向を選んでください"
+      puts "--------------"
       $janken_result = "lose"
-      return false
+      $player_hand += 1
     end
   end
 end
@@ -84,6 +90,9 @@ def achimuite
         if player_hoi == program_hoi
           puts "あなたの勝ち！"
           $hoi_result = "win"
+        else
+          $hoi_result = "draw"
+          player_hoi += 1
         end
       end
   when "program_turn"
@@ -101,23 +110,26 @@ def achimuite
         
         puts "あなたの顔：" + achimuites[player_hoi] 
         puts "相手の指：" + achimuites[program_hoi]
+        puts "--------------"
         if player_hoi == program_hoi
           puts "あなたの負け..."
           $hoi_result = "lose"
+        else
+          $hoi_result = "draw"
+          player_hoi += 1
         end
       end
   end
 end
 
 janken
-if $janken_result == "win" || $janken_result == "lose"
-  achimuite
-end
-if $hoi_result == "win" || $hoi_result == "lose"
-  return true
-else 
-  while $hoi_result != "win" && $hoi_result != "lose"
+achimuite
+while $hoi_result == "draw"
   janken
+  if $player_hand == 4
+    return true
+  elsif $player_hand > 4 || $player_hand < 1
+    return true
+  end  
   achimuite
-  end
 end
